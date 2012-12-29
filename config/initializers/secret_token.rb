2fb38fdd25f152015064dcb868aa6107ece85a43
yuki imada <desandimg@gmail.com>
2012-12-29 19:41:08 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YukiApp::Application.config.secret_token = 'a734ed3f236fad45d619b07045fdc6e494cd7100084c65be70920891ce57e2e8df189ebcce76192cc3cd01dd941a97df040cb60dcf63944e1bb9934d4897bb75'
